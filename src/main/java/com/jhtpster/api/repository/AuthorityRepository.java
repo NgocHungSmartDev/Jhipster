be6c2d9817981100ec34841114b4f564398a81a8
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-02-18 17:41:09 +0700

    init

---
+++ src/main/java/com/jhtpster/api/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.jhtpster.api.repository;
+
+import com.jhtpster.api.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
