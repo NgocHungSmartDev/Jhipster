476fc4e7a45bec4c92a750bac67c27c2661355d6
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-02-19 10:14:57 +0700

    Initial version of apiGatewayBE generated by generator-jhipster@7.9.3

---
+++ Microservice application/src/main/webapp/declarations.d.ts
@@ -0,0 +1,13 @@
+declare const SERVER_API_URL: string;
+declare const I18N_HASH: string;
+
+declare module 'microfe/entity-routing' {
+  export class EntityRoutingModule {}
+}
+declare module 'microfe/entity-navbar-items' {
+  export const EntityNavbarItems: [];
+}
+
+declare module 'microfe/translation-module' {
+  export class LazyTranslationModule {}
+}
